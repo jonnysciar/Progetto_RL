90a7a29c6a2086c935192a347a1e8bf9c8f14b6e
Jonathan Sciarrabba <jonathan.sciarrabba@mail.polimi.it>
2022-03-08 18:45:05 +0100

    FSM in procedure

--- FSM.vhd
+++ FSM.vhd
@@ -20,83 +20,85 @@ end project_reti_logiche;
 architecture rtl of project_reti_logiche is
     
     type t_state is (state00, state01, state10, state11);
-    signal state : t_state := state00;
-    signal test : std_logic_vector(15 downto 0);
+    signal STATE : t_state := state00;
     signal firstpart : std_logic_vector(7 downto 0);
     signal secondpart : std_logic_vector(7 downto 0);
     
-begin
-    o_en <= '1';
-    o_address <= x"0002";
-    process(i_rst, i_start)
-    begin
-        if rising_edge(i_rst) or rising_edge(i_start) then
-            --state <= state00;
-        end if;
-    end process;
-    
-    process --(i_clk)
-        variable data : std_logic_vector(7 downto 0) := "10100010";
-        variable output_data : std_logic_vector(15 downto 0);
-        variable state_int : t_state;
+    procedure convolutional_12 (signal data_in : in std_logic_vector(7 downto 0);
+                                signal state : inout t_state;
+                                signal byte1 : out std_logic_vector(7 downto 0);
+                                signal byte2 : out std_logic_vector(7 downto 0)) is
+        
+        variable state_var : t_state;
+        variable full_output : std_logic_vector(15 downto 0);
+        
         variable pk1 : integer;
         variable pk2 : integer;
-    begin
-        --if rising_edge(i_clk) then
-            --data := i_data;
-            state_int := state;
+        
+        begin
+            state_var := state;
             for i in 7 downto 0 loop
                 pk1 := (2*i) + 1;
                 pk2 := 2*i;
-                case state_int is
+                case state_var is
                     when state00 =>
-                        if data(i) = '0' then
-                            state_int := state00;
-                            output_data(pk1) := '0';
-                            output_data(pk2) := '0';
+                        if data_in(i) = '0' then
+                            state_var := state00;
+                            full_output(pk1) := '0';
+                            full_output(pk2) := '0';
                         else
-                            state_int := state10;
-                            output_data(pk1) := '1';
-                            output_data(pk2) := '1';
+                            state_var := state10;
+                            full_output(pk1) := '1';
+                            full_output(pk2) := '1';
                         end if;
                     when state10 =>
-                        if data(i) = '0' then
-                            state_int := state01;
-                            output_data(pk1) := '0';
-                            output_data(pk2) := '1';
+                        if data_in(i) = '0' then
+                            state_var := state01;
+                            full_output(pk1) := '0';
+                            full_output(pk2) := '1';
                         else
-                            state_int := state11;
-                            output_data(pk1) := '1';
-                            output_data(pk2) := '0'; 
+                            state_var := state11;
+                            full_output(pk1) := '1';
+                            full_output(pk2) := '0'; 
                         end if;
                     when state01 =>
-                        if data(i) = '0' then
-                            state_int := state00;
-                            output_data(pk1) := '1';
-                            output_data(pk2) := '1';
+                        if data_in(i) = '0' then
+                            state_var := state00;
+                            full_output(pk1) := '1';
+                            full_output(pk2) := '1';
                         else
-                            state_int := state10;
-                            output_data(pk1) := '0';
-                            output_data(pk2) := '0'; 
+                            state_var := state10;
+                            full_output(pk1) := '0';
+                            full_output(pk2) := '0'; 
                         end if;
                     when state11 =>
-                        if data(i) = '0' then
-                            state_int := state01;
-                            output_data(pk1) := '1';
-                            output_data(pk2) := '0';
+                        if data_in(i) = '0' then
+                            state_var := state01;
+                            full_output(pk1) := '1';
+                            full_output(pk2) := '0';
                         else
-                            state_int := state11; 
-                            output_data(pk1) := '0';
-                            output_data(pk2) := '1';
+                            state_var := state11; 
+                            full_output(pk1) := '0';
+                            full_output(pk2) := '1';
                         end if;
                 end case;
             end loop;
-            state <= state_int;
-            test <= output_data;
-            firstpart <= output_data(15 downto 8);
-            secondpart <= output_data(7 downto 0);
-       -- end if;
-        wait for 2000 ns;
+            state <= state_var;
+            byte1 <= full_output(15 downto 8);
+            byte2 <= full_output(7 downto 0);
+        end procedure;
+    
+begin
+    o_en <= '1';
+    o_address <= x"0001";
+    
+    process(i_start)
+    
+    begin
+        if rising_edge(i_start) then
+            convolutional_12(i_data, STATE, firstpart, secondpart);
+        end if;
     end process;
+    
     
 end architecture;
